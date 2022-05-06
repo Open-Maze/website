6b0b5b6703dcadda5363630bc2090f39145293bf
Jordy Arntz <hello@jordyarntz.com>
2022-05-06 16:30:14 +0200

    Fix testing error

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.scss';
 
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Nav from '../Nav/Nav';
 import Home from '../../pages/Home';
 
 const App = () => (
   <div className="app">
-    <Nav />
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-    </Routes>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </div>
 );
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App/App';
 
@@ -16,9 +15,7 @@ const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
