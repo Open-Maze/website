2b0047f1a970dc550983b7943cbe84582844421f
Stijn Verhagen <stijn@moonlysoftware.com>
2022-05-09 15:11:14 +0200

    Start on header and color variables.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,22 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <div className="nav">
-    <Link to="/">Home</Link>
+    <div className="left">
+      <img src="" alt="Logo" />
+    </div>
+    <div className="right">
+      <div className="menu">
+        <Link to="/">home</Link>
+        <Link to="/">products</Link>
+        <Link to="/">about</Link>
+        <Link to="/">contact</Link>
+      </div>
+      <div className="cta">
+        <Link to="/" className="cta_button purple">
+          Schedule demo
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
