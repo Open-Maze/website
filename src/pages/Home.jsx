cae2bb432a5e7b8af69f5a58b36fab84ad1a7a32
Jordy Arntz <hello@jordyarntz.com>
2022-05-23 15:17:17 +0200

    Remove comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './shared.scss';
 
-// import useFetch from '../core/useFetch';
-
 const Home = () => (
-  // const { loading, error, data } = useFetch('http://localhost:1337/');
-
   <div className="page home">
     <h1>Welcome at OpenMaze</h1>
     <br />
