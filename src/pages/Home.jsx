1fb0be7b812fceaa1e4f1119b0786d4209092a5c
Jordy Arntz <hello@jordyarntz.com>
2022-05-23 15:46:58 +0200

    Prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => (
     <h1>Welcome at OpenMaze</h1>
     <br />
     <h4>Lorem ipsum</h4>
-
     <h2>Our core values</h2>
     <p>Wij zijn OpenMaze, en dit zijn onze belangrijkste uitgangspunten</p>
   </div>
 );
+
 export default Home;
