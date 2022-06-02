3402d8c73102c627aa1bfce4f01f9f23013edc8b
Stijn Verhagen <stijn@moonlysoftware.com>
2022-06-02 14:33:30 +0200

    Structure, styling, functionality, responsive

--- src/components/HeaderL/HeaderL.jsx
+++ src/components/HeaderL/HeaderL.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './HeaderL.scss';
 import Button from '../Button/Button';
-import headerSvg from '../../assets/images/illustrations/header-l.svg';
+import headerSvg from '../../assets/images/illustrations/header.svg';
 
 const HeaderL = () => (
   <section className="header_l">
---
+++ src/components/HeaderS/HeaderS.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './HeaderS.scss';
+import Button from '../Button/Button';
+
+const HeaderS = ({ title, description, buttonLabel, buttonLink, svg }) => (
+  <section className="header_s">
+    <div className="left">
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <div className="buttons">
+        <Button onClick={buttonLink} className="btn-purple-solid">
+          {buttonLabel}
+        </Button>
+      </div>
+    </div>
+    <div className="right">
+      <img src={svg} alt="Illustration" />
+    </div>
+  </section>
+);
+
+export default HeaderS;
