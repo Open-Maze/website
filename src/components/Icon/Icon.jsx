3808da4aaabd9472e9095665209f89e8f44bd409
Jordy Arntz <hello@jordyarntz.com>
2022-06-02 10:50:21 +0200

    Start icon component

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Icon.scss';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
+
+const Icon = ({ front, back }) => {
+  const iconList = Object.keys(Icons)
+    .filter((key) => key !== 'fas' && key !== 'prefix')
+    .map((icon) => Icons[icon]);
+
+  library.add(...iconList);
+
+  return (
+    <div className="icon">
+      <FontAwesomeIcon className="icon-front" icon={front} />
+      <FontAwesomeIcon className="icon-back" icon={back} />
+    </div>
+  );
+};
+
+export default Icon;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './shared.scss';
 
+import Icon from '../components/Icon/Icon';
+
 const Home = () => (
   <div className="page home">
     <h1>Welcome at OpenMaze</h1>
@@ -8,6 +10,7 @@ const Home = () => (
     <h4>Lorem ipsum</h4>
     <h2>Our core values</h2>
     <p>Wij zijn OpenMaze, en dit zijn onze belangrijkste uitgangspunten</p>
+    <Icon front="heart" back="circle" />
   </div>
 );
 
