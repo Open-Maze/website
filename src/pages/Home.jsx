5d223928a7573293004c6bbcfb0f8d8a28567d23
Jordy Arntz <hello@jordyarntz.com>
2022-06-02 12:36:53 +0200

    Fix build errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,32 @@ import './shared.scss';
 import Input from '../components/Input/Input';
 import Button from '../components/Button/Button';
 
-const Home = () => (
-  <div className="page home">
-    <h1>Welcome at OpenMaze</h1>
-    <br />
-    <h4>Lorem ipsum</h4>
-    <h2>Our core values</h2>
-    <p>Wij zijn OpenMaze, en dit zijn onze belangrijkste uitgangspunten</p>
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
 
-    <Button className="btn-purple-solid">Learn more</Button>
-    <br />
-    <br />
-    <Button className="btn-purple-outline">Learn more</Button>
+  return (
+    <div className="page home">
+      <h1>Welcome at OpenMaze</h1>
+      <br />
+      <h4>Lorem ipsum</h4>
+      <h2>Our core values</h2>
+      <p>Wij zijn OpenMaze, en dit zijn onze belangrijkste uitgangspunten</p>
 
-    <Input
-      value={inputValue}
-      handleChange={(event) => {
-        setInputValue(event.target.value);
-      }}
-      label="Voornaam"
-      placeholder="Stijn"
-    />
-  </div>
-);
+      <Button className="btn-purple-solid">Learn more</Button>
+      <br />
+      <br />
+      <Button className="btn-purple-outline">Learn more</Button>
+
+      <Input
+        value={inputValue}
+        handleChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+        label="Voornaam"
+        placeholder="Stijn"
+      />
+    </div>
+  );
+};
 
 export default Home;
