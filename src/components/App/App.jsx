d925ea675386cebb34bc1de14b1dd279e43de6c0
selinewarners <selinewarners@gmail.com>
2022-06-07 10:03:58 +0200

    Font fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { createMemoryHistory } from 'history';
 
 import Nav from '../Nav/Nav';
