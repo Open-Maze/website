2ca73acf47540a4d9b99fe6dfd12093b6c203687
selinewarners <selinewarners@gmail.com>
2022-06-07 09:56:58 +0200

    Fixed buttons with correct routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,8 +23,8 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </Router>
-      <Footer />
     </div>
   );
 };
