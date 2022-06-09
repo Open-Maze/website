f0895f375424f1eb94851b1e76a3ff3244b9e379
Stijn Verhagen <stijn@moonlysoftware.com>
2022-06-09 11:51:22 +0200

    Created feature component - responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,10 @@ import Button from '../components/Button/Button';
 import Icon from '../components/Icon/Icon';
 import HeaderL from '../components/HeaderL/HeaderL';
 import Information from '../components/Information/Information';
+import FeatureComponent from '../components/FeatureComponent/FeatureComponent';
 
 import Svg from '../assets/images/illustrations/header.svg';
+import IphonePlaceholder from '../assets/images/iphone-placeholder.png';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
@@ -46,6 +48,12 @@ const Home = () => {
         buttonLink="/about"
         image={Svg}
         flipped
+      />
+      <FeatureComponent
+        featureNr="Feature 1"
+        title="Instant feedback"
+        description="The Document Feedback Tool allows students to have their documents fully reviewed immediately, before they are graded by teachers. The purpose of the feedback tool is to eliminate that first round of feedback and allow the teacher and student to engage in meaningful dialogue."
+        image={IphonePlaceholder}
       />
       <br />
       <br />
