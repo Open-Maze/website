f8b55f860ed26128260319977474b7f19837afec
Jordy Arntz <hello@jordyarntz.com>
2022-06-20 12:26:02 +0200

    Fix width

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Contact.scss';
+
 import Button from '../Button/Button';
 
 const Contact = () => (
-  <section className="contact-area">
-    <div className="left">
-      <h2>Get in touch</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. aliquam ac commodo erat, quis venenatis turpis. Vivamus
-        interdum interdum.
-      </p>
-      <div className="buttons">
-        <Button pageUrl="/contact" className="btn-purple-solid">
-          Call now
-        </Button>
+  <div className="contact">
+    <div className="contact-area">
+      <div className="left">
+        <h2>Get in touch</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. aliquam ac commodo erat, quis venenatis turpis.
+          Vivamus interdum interdum.
+        </p>
+        <div className="buttons">
+          <Button pageUrl="/contact" className="btn-purple-solid">
+            Call now
+          </Button>
+        </div>
       </div>
+      <div className="right" />
     </div>
-    <div className="right" />
-  </section>
+  </div>
 );
 export default Contact;
