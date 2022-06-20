9ece775af2d7ef4b353201c04b08fbbbddf04e2d
Jordy Arntz <hello@jordyarntz.com>
2022-06-20 16:06:49 +0200

    Flippable

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -4,21 +4,28 @@ import './Information.scss';
 import Button from '../Button/Button';
 
 const Information = ({ title, description, buttonLabel, buttonLink, image, flipped }) => {
-  let flexDirection;
+  let orderLeft;
+  let orderRight;
   if (flipped) {
-    flexDirection = {
-      flexDirection: 'row-reverse',
+    orderLeft = {
+      order: '1',
+    };
+    orderRight = {
+      order: '2',
     };
   } else {
-    flexDirection = {
-      flexDirection: 'row',
+    orderLeft = {
+      order: '2',
+    };
+    orderRight = {
+      order: '1',
     };
   }
 
   return (
     <div className="information">
-      <div className="information-wrapper" style={flexDirection}>
-        <div className="left">
+      <div className="information-wrapper">
+        <div className="left" style={orderLeft}>
           <h1>{title}</h1>
           <p>{description}</p>
           <div className="buttons">
@@ -27,7 +34,7 @@ const Information = ({ title, description, buttonLabel, buttonLink, image, flipp
             </Button>
           </div>
         </div>
-        <div className="right">
+        <div className="right" style={orderRight}>
           <img src={image} alt="Illustration" />
         </div>
       </div>
