117e8d63ce6e013f34fb2778e4051a4e257355da
Jordy Arntz <hello@jordyarntz.com>
2022-06-20 12:12:11 +0200

    Create core values

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,12 +14,30 @@ const App = () => (
     <Nav />
     <div className="page">
       <HeaderL />
-      <CardIconVertical
-        iconFront="rocket"
-        iconBack="circle"
-        title="Progression"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      />
+      <div className="core-values">
+        <h4>Lorem ipsum</h4>
+        <h2>Our core values</h2>
+        <div className="core-values-cards">
+          <CardIconVertical
+            iconFront="rocket"
+            iconBack="circle"
+            title="Progression"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <CardIconVertical
+            iconFront="rocket"
+            iconBack="circle"
+            title="Progression"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <CardIconVertical
+            iconFront="rocket"
+            iconBack="circle"
+            title="Progression"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+        </div>
+      </div>
       <Information
         title="About us"
         description="OpenMaze aims to provide users of online educational environments with greater insight into their own learning journey and performance through the use of AI-based tools. The use of such tooling often means that people working in the educational sector have to do less menial tasks."
