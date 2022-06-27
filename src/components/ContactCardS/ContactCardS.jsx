d8e6a7303feb7aaf00279fe8cb0843ad31cb42cb
Jordy Arntz <hello@jordyarntz.com>
2022-06-27 14:59:01 +0200

    Fix hover Contact Cards

--- src/components/ContactCardS/ContactCardS.jsx
+++ src/components/ContactCardS/ContactCardS.jsx
@@ -4,12 +4,14 @@ import './ContactCardS.scss';
 const ContactCardS = ({ link, icon, title, content, delay }) => (
   <a href={link} target="_blank" rel="noreferrer">
     <div className="ContactCardS" data-aos="fade-up" data-aos-delay={delay}>
-      <div className={`left ${icon}`} />
-      <div className="middle">
-        <p>{title}</p>
-      </div>
-      <div className="right">
-        <p>{content}</p>
+      <div className="ContactCardS-inner">
+        <div className={`left ${icon}`} />
+        <div className="middle">
+          <p>{title}</p>
+        </div>
+        <div className="right">
+          <p>{content}</p>
+        </div>
       </div>
     </div>
   </a>
