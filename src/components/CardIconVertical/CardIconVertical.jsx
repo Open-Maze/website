e2b052e0f58e4ae68d206b59d012b26690677c83
Jordy Arntz <hello@jordyarntz.com>
2022-09-16 13:57:01 +0200

    Core values tailwind

--- src/components/CardIconVertical/CardIconVertical.jsx
+++ src/components/CardIconVertical/CardIconVertical.jsx
@@ -6,7 +6,7 @@ import Icon from '../Icon/Icon';
 const CardIconVertical = ({ iconFront, iconBack, title, text, delay }) => (
   <div className="w-full rounded-lg bg-white p-10" data-aos="fade-up" data-aos-delay={delay}>
     <Icon front={iconFront} back={iconBack} />
-    <h3>{title}</h3>
+    <h3 className="py-6">{title}</h3>
     <p className="text-slate-700">{text}</p>
   </div>
 );
