d1407d0cfb5c2e36da712eee811ae99a2004802a
Jordy Arntz <hello@jordyarntz.com>
2022-09-21 14:24:48 +0200

    Finish footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 // External
 import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 import moment from 'moment';
 
 // Assets
 import { ReactComponent as LogoPurpleBlue } from '../../assets/images/logo/logoPurpleBlue.svg';
+
+// Font Awesome Library
+library.add(faLinkedinIn);
 
 const Footer = () => {
   const location = useLocation();
@@ -14,42 +20,45 @@ const Footer = () => {
 
   return (
     <div className="footer">
-      <div className="flex w-full flex-col items-stretch justify-start border-b-2 border-solid border-bleach bg-white px-10">
+      <div className="flex w-full flex-col items-start border-b-2 border-solid border-bleach bg-white px-4 text-left sm:flex-row sm:items-center sm:justify-between sm:border-y-2 md:px-10">
         <div>
           {location.pathname === '/' && (
             <a href="#top">
-              <LogoPurpleBlue />
+              <LogoPurpleBlue className="mt-2 h-6" />
             </a>
           )}
           {location.pathname !== '/' && (
             <Link to="/">
-              <LogoPurpleBlue />
+              <LogoPurpleBlue className="mt-2 h-6" />
             </Link>
           )}
         </div>
-        <div className="flex h-full items-center">
-          <a
-            href="https://linkedin.com/company/open-maze"
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center"
-          >
-            <FontAwesomeIcon icon="circle" className="h-6 px-6 text-violet transition hover:text-violet-darker" />
-          </a>
-          <a href="#top" className="hidden lg:block">
-            <div className="flex aspect-square items-center justify-center border-x-2 border-solid border-bleach">
-              <FontAwesomeIcon icon="circle" className="h-6 px-6 text-violet transition hover:text-violet-darker" />
+        <div className="flex h-full w-full items-center justify-between sm:justify-end">
+          <div className="flex">
+            <a
+              href="https://linkedin.com/company/open-maze"
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center"
+            >
+              <FontAwesomeIcon
+                icon="fa-brands fa-linkedin-in"
+                className="h-6 pr-6 text-violet transition hover:text-violet-darker"
+              />
+            </a>
+          </div>
+          <a href="#top">
+            <div className="flex items-center justify-center py-6 pl-6 sm:border-l-2 sm:border-solid sm:border-bleach">
+              <FontAwesomeIcon
+                icon="circle-arrow-up"
+                className="aspect-square h-6 text-violet transition hover:text-violet-darker"
+              />
             </div>
           </a>
         </div>
       </div>
-      <div className="flex h-20 w-full items-center justify-center bg-white px-12 text-slate-400">
+      <div className="flex h-20 w-full items-center justify-center bg-white px-4 text-slate-400">
         <p>Copyright &copy; {year} OpenMaze</p>
-        <a href="#top" className="ml-auto block lg:hidden">
-          <div>
-            <FontAwesomeIcon icon="circle" className="h-6 px-6 text-violet transition hover:text-violet-darker" />
-          </div>
-        </a>
       </div>
     </div>
   );
