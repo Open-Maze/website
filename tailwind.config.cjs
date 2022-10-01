4eaa1f37a8c7a7ccf3d11d79120a53cf0809adae
Jordy Arntz <hello@jordyarntz.com>
2022-10-01 20:17:38 +0200

    Fix container

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
         transparent: 'transparent',
