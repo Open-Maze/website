995e6ea6a297bec27867c772b3b2faf05d97c9d4
Jordy Arntz <hello@jordyarntz.com>
2022-10-28 13:07:35 +0200

    Fix Icon component

--- src/components/CoreValues/CoreValues.jsx
+++ src/components/CoreValues/CoreValues.jsx
@@ -15,8 +15,8 @@ const CoreValues = () => (
       <div className="mt-20 grid grid-cols-1 gap-10 lg:grid-cols-12">
         <div className="col-span-4">
           <CardIconVertical
-            iconFront="rocket"
-            iconBack="circle"
+            iconFront="fa-solid fa-linkedin-in"
+            iconBack="fa-solid fa-circle"
             title="Cutting edge"
             text="We differ ourselves from existing EdTech companies by implementing state of the art technologies, broadening the diversity of tools available. We unite context and technology."
             delay={200}
@@ -24,8 +24,8 @@ const CoreValues = () => (
         </div>
         <div className="col-span-4">
           <CardIconVertical
-            iconFront="shield-alt"
-            iconBack="circle"
+            iconFront="fa-solid fa-shield-alt"
+            iconBack="fa-solid fa-circle"
             title="Privacy"
             text="We are of the opinion that data ownership should always remain with the user. We not only comply with regulations, but go the extra mile to ensure that you control your data."
             delay={300}
@@ -33,8 +33,8 @@ const CoreValues = () => (
         </div>
         <div className="col-span-4">
           <CardIconVertical
-            iconFront="heart"
-            iconBack="circle"
+            iconFront="fa-solid fa-heart"
+            iconBack="fa-solid fa-circle"
             title="Integrity"
             text="AI can be scary. We think that technology should always be transparent and explainable. Our technology should above all be trusted by its users."
             delay={400}
