7fc60e79a70b1d71ab66aede3d752698c4e80762
Jordy Arntz <hello@jordyarntz.com>
2022-11-04 09:22:54 +0100

    Update deps

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
   optimizeDeps: {
     esbuildOptions: {
       plugins: [esbuildCommonjs(['react-moment'])],
