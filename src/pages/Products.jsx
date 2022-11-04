11e0c61021cf16a111e1798ebed038add8b6264a
Stijn Verhagen <stijn@moonlysoftware.com>
2022-11-04 12:18:51 +0100

    Show card for preview

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './shared.css';
+import FeatureComponent from '../components/FeatureComponent/FeatureComponent';
+import CardImageHorizontal from '../components/CardImageHorizontal/CardImageHorizontal';
+import Placeholder from '../assets/images/iphone-placeholder.png';
 
 const Products = () => (
   <div className="page products">
     <h1>Products</h1>
+    <FeatureComponent
+      featureNr="Feature 1"
+      title="Instant feedback"
+      description="The Document Feedback Tool allows students to have their documents fully reviewed immediately, before they are graded by teachers. The purpose of the feedback tool is to eliminate that first round of feedback and allow the teacher and student to engage in meaningful dialogue."
+      image={Placeholder}
+    />
+    <CardImageHorizontal
+      imageLeft
+      title="Instant feedback"
+      description="The Document Feedback Tool allows students to have their documents fully reviewed immediately, before they are graded by teachers. The purpose of the feedback tool is to eliminate that first round of feedback and allow the teacher and student to engage in meaningful dialogue."
+      image={Placeholder}
+      buttonLabel="Contact us"
+      buttonLink="/contact"
+    />
   </div>
 );
 
