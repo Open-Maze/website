d68454b1d09f93623774c9135bfe273b0a1ef5e3
Jordy Arntz <hello@jordyarntz.com>
2022-11-11 11:07:18 +0100

    Setup component

---
+++ src/components/CTA/CTA.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CTA = () => (
+  <div className="cms-block">
+    <div className="container">
+      <div className="grid grid-cols-12 gap-10 border-t border-midnight pt-20">
+        <div className="col-span-4 col-start-1">
+          <h4 className="pb-2 text-dark" data-aos="fade-up">
+            Want to talk?
+          </h4>
+          <h2 data-aos="fade-up" data-aos-delay="100">
+            Contact us
+          </h2>
+        </div>
+        <div className="col-span-8 col-end-13" data-aos="fade-up" data-aos-delay="200">
+          test
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default CTA;
