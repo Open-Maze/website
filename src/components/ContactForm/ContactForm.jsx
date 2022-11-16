e5d5e90e62b8bb2ba4e7647779bb24ecb20febce
Stijn Verhagen <stijn@moonlysoftware.com>
2022-11-16 13:07:07 +0100

    Contact form back-end

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,115 +1,84 @@
 import { React, useState } from 'react';
 import axios from 'axios';
-import Input from '../Input/Input';
 
 const initialState = {
-  name: '',
+  fullName: '',
   companyName: '',
   email: '',
   phone: '',
   message: '',
 };
 
-function signupUser() {
-  axios({
-    method: 'POST',
-    url: '/contact-form.php',
-    data: { ...initialState },
-  }).then((response) => {
-    if (response.data.status === 'success') {
-      alert('Message Sent.');
-      this.resetForm();
-    } else if (response.data.status === 'fail') {
-      alert('Message failed to send.');
-    }
-  });
-}
-
 const ContactForm = () => {
-  const [{ name, companyName, email, phone, message }, setState] = useState(initialState);
+  const [data, setData] = useState({
+    fullName: '',
+    companyName: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+
+  function sendMessage() {
+    const API_PATH = 'http://127.0.0.1:5173/api/contact/index.php';
+    axios({
+      method: 'POST',
+      url: `${API_PATH}`,
+      headers: { 'content-type': 'application/json' },
+      data: { data },
+    }).then((response) => {
+      console.log(response);
+      if (response.data.sent) {
+        console.log('Message Sent.');
+      } else if (response.data.sent === false) {
+        console.log('kapot');
+      }
+    });
+  }
 
   const clearState = () => {
-    setState({ ...initialState });
+    setData({ ...initialState });
   };
 
   const onChange = (e) => {
     const { name, value } = e.target;
-    setState((prevState) => ({ ...prevState, [name]: value }));
+    setData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    signupUser();
+    console.log(data);
+    sendMessage();
     clearState();
   };
 
   return (
     <div className="App">
       <form id="contact-form" onSubmit={handleSubmit} method="POST">
-        <div className="form-group">
-          <Input
-            label=""
-            handleChange={onChange}
-            value={name}
-            placeholder="Voor- en achternaam"
-            outline=""
-            white
-            fullwidth=""
-            inputType=""
-          />
-          <input
-            placeholder="Voor- en achternaam*"
-            type="text"
-            className="form-control"
-            id="naam"
-            value={name}
-            name="name"
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            placeholder="Bedrijfsnaam"
-            type="text"
-            className="form-control"
-            id="bedrijfsnaam"
-            aria-describedby="companyHelp"
-            value={companyName}
-            name="companyName"
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            placeholder="E-mail*"
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            value={email}
-            name="email"
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            placeholder="Telefoon"
-            type="phone"
-            className="form-control"
-            id="telefoon"
-            aria-describedby="telefoonHelp"
-            value={phone}
-            name="phone"
-            onChange={onChange}
-          />
-        </div>
+        {[
+          ['fullName', data.fullName, 'Voor- en achternaam*', 'text'],
+          ['companyName', data.companyName, 'bedrijfsnaam', 'text'],
+          ['email', data.email, 'E-mail*', 'email'],
+          ['phone', data.phone, 'Telefoon', 'phone'],
+        ].map(([label, iValue, placeholder, type]) => (
+          <div className="form-group">
+            <input
+              placeholder={placeholder}
+              type={type}
+              className="form-control"
+              id={label}
+              value={iValue}
+              name={label}
+              onChange={onChange}
+            />
+          </div>
+        ))}
         <div className="form-group">
           <textarea
             placeholder="Bericht"
             className="form-control"
             rows="5"
             id="bericht"
-            value={message}
+            value={data.message}
             name="message"
             onChange={onChange}
           />
