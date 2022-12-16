9d9d6c3378a16f6bd17351f7f68d872b363754c3
Jordy Arntz <hello@jordyarntz.com>
2022-12-16 10:04:41 +0100

    Add contact buttons to Contact page

--- src/blocks/Header/Header.jsx
+++ src/blocks/Header/Header.jsx
@@ -7,7 +7,19 @@ import Button from '../../components/Button/Button';
 // Assets
 import { ReactComponent as ScrollDown } from '../../assets/images/icons/chevron-down.svg';
 
-const Header = ({ size, title, text, buttonLink, buttonLabel, button2Link, button2Label, image, external, arrow }) => {
+const Header = ({
+  size,
+  title,
+  text,
+  buttonLink,
+  buttonLabel,
+  button2Link,
+  button2Label,
+  image,
+  external,
+  arrow,
+  children,
+}) => {
   function scrollDown() {
     window.scroll(0, window.scrollY + 400);
   }
@@ -16,7 +28,7 @@ const Header = ({ size, title, text, buttonLink, buttonLabel, button2Link, butto
     <>
       <div className={`cms-block flex items-center bg-white ${size}`}>
         <div className="container grid grid-cols-1 items-center gap-10 pt-24 lg:grid-cols-12">
-          <div className="col-span-5 lg:col-start-1">
+          <div className="col-span-1 lg:col-span-5 lg:col-start-1">
             {title && (
               <h1 className="pb-6" data-aos="fade-up" data-aos-delay="300">
                 <mark>{title}</mark>
@@ -27,25 +39,28 @@ const Header = ({ size, title, text, buttonLink, buttonLabel, button2Link, butto
                 <mark>{text}</mark>
               </p>
             )}
-            <div className="flex flex-col gap-6 sm:flex-row">
-              {buttonLink && (
-                <div className="buttons" data-aos="fade-up" data-aos-delay="500">
-                  <Button external={external} pageUrl={buttonLink} className="button button--violet">
-                    {buttonLabel}
-                  </Button>
-                </div>
-              )}
-              {button2Link && (
-                <div className="buttons" data-aos="fade-up" data-aos-delay="500">
-                  <Button external={external} pageUrl={button2Link} className="button button--violet-outline">
-                    {button2Label}
-                  </Button>
-                </div>
-              )}
-            </div>
+            {!children && (
+              <div className="flex flex-col gap-6 sm:flex-row">
+                {buttonLink && (
+                  <div className="buttons" data-aos="fade-up" data-aos-delay="500">
+                    <Button external={external} pageUrl={buttonLink} className="button button--violet">
+                      {buttonLabel}
+                    </Button>
+                  </div>
+                )}
+                {button2Link && (
+                  <div className="buttons" data-aos="fade-up" data-aos-delay="500">
+                    <Button external={external} pageUrl={button2Link} className="button button--violet-outline">
+                      {button2Label}
+                    </Button>
+                  </div>
+                )}
+              </div>
+            )}
+            {children && children}
           </div>
           {image && (
-            <div className="col-span-6 h-full lg:col-end-13" data-aos="fade-up" data-aos-delay="300">
+            <div className="col-span-1 h-full lg:col-span-6 lg:col-end-13" data-aos="fade-up" data-aos-delay="300">
               <img src={image} alt={title} className="mx-auto mt-12 h-full lg:mt-0" />
             </div>
           )}
